2da78c02663f037e005ddf3300254e221f887991
Byambasuren11 <165979583+Byambasuren11@users.noreply.github.com>
2025-01-07 13:41:25 +0800

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
 import * as React from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 function App() {
   const [task, setTask] = useState([]);
   const [inputValue, setInputValue] = useState();
+  const [filterState, setFilterState] = useState("all");
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
   const addTask = () => {
-    setTask((task) => [...task, { name: inputValue, status: "active", id:uuidv4() }]);
-     setInputValue("");
+    setTask((task) => [
+      ...task,
+      { name: inputValue, status: "active", id: uuidv4() },
+    ]);
+    setInputValue("");
+  };
+  console.log(task);
+  const handleTasksChange = (id) => {
+    console.log(id);
+    const tasks = task.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, status: "completed" };
+      } else {
+        return todo;
+      }
+    });
+    setTask(tasks);
+  };
+  const handleFilterStateChange = (state) => {
+    setFilterState(state);
+    //console.log(state);
   };
   return (
     <>
@@ -23,7 +43,6 @@ function App() {
               id="input"
               className="inputToDo"
               value={inputValue}
-
               onChange={handleInputChange}
             />
             <button className="addButton" onClick={addTask}>
@@ -31,9 +50,33 @@ function App() {
             </button>
           </div>
           <div className="categorize">
-            <button className="allList">All</button>
-            <button className="activeList">Active</button>
-            <button className="completedList">Complated</button>
+            <button
+              className="allList"
+              onClick={() => handleFilterStateChange("all")}
+              style={{
+                background: filterState === "all" ? "#3c82f6" : "#f3f4f6",
+              }}
+            >
+              All
+            </button>
+            <button
+              className="activeList"
+              onClick={() => handleFilterStateChange("active")}
+              style={{
+                background: filterState === "active" ? "#3c82f6" : "#f3f4f6",
+              }}
+            >
+              Active
+            </button>
+            <button
+              className="completedList"
+              onClick={() => handleFilterStateChange("completed")}
+              style={{
+                background: filterState === "completed" ? "#3c82f6" : "#f3f4f6",
+              }}
+            >
+              Complated
+            </button>
           </div>
         </div>
         <div className="noTaskYet">
@@ -42,13 +85,23 @@ function App() {
           ) : (
             <div>
               <p className="todoList">
-                {task.map((element, i) => (
-                  <p className="todo" key={i}>
-                    <input type="checkbox" 
-                    />
-                    {element.name}
-                  </p>
-                ))}
+                {task
+                  .filter((element) => {
+                    if (filterState === "all") {
+                      return true;
+                    } else if (filterState === element.status) {
+                      return element;
+                    }
+                  })
+                  .map((element) => (
+                    <p className="todo">
+                      <input
+                        type="checkbox"
+                        onChange={() => handleTasksChange(element.id)}
+                      />
+                      {element.name}
+                    </p>
+                  ))}
               </p>
             </div>
           )}
